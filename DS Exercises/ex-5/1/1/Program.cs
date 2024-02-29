c06a17e36829521516460318f3ce9738426e0ceb
Alibest3 <alibest3@yahoo.com>
2024-02-29 15:02:21 +0330

    DS exercise 5 added

---
+++ DS Exercises/ex-5/1/1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int listLength = Convert.ToInt32(Console.ReadLine());
+            List<string> inputList = new List<string>();
+
+            for (int i = 0;i< listLength;i++)
+            {
+                inputList.Add((Console.ReadLine()));
+            }
+
+
+            BiggestNumber.FindBiggestNumber(inputList);
+            Console.ReadLine();
+        }
+    }
+        class BiggestNumber
+        {
+            public static void FindBiggestNumber(List<string> inputList)
+            {
+                string result = "";
+
+                inputList.Sort(Compare);
+
+                for (int i = 0; i < inputList.Count; i++)
+                {
+                    result = result + inputList[i];
+                }
+                if (result[0] == '0' && result.Length > 1)
+                {
+                    Console.WriteLine("0");
+                }
+            else
+            {
+                Console.WriteLine(result);
+            }
+
+        }
+
+            internal static int Compare(string a, string b)
+            {
+                
+                string ab = a + b;
+                string ba = b + a;
+
+                return ab.CompareTo(ba) > 0 ? -1 : 1;
+            }
+
+            
+        }
+    
+}
+
---
+++ DS Exercises/ex-5/2/2/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    internal class Program
+    {
+        static int mergeSort(int[] inputArray, int arrayLength)
+        {
+            int[] temp = new int[arrayLength];
+
+            return mergeSort2(inputArray, temp, 0, arrayLength - 1);
+        }
+
+        static int mergeSort2(int[] inputArray, int[] temp, int left, int right)
+        {
+            int mid, invCount = 0;
+            if (right > left)
+            {
+                mid = (right + left) / 2;
+
+                invCount += mergeSort2(inputArray, temp, left, mid);
+                invCount += mergeSort2(inputArray, temp, mid + 1, right);
+
+                invCount += merge(inputArray, temp, left, mid + 1, right);
+            }
+            return invCount;
+        }
+        static int merge(int[] inputArray, int[] temp, int left, int mid, int right)
+        {
+            int i, j, k;
+            int invCount = 0;
+
+            i = left; 
+            j = mid; 
+            k = left; 
+                     
+            while ((i <= mid - 1) && (j <= right))
+            {
+                if (inputArray[i] <= inputArray[j])
+                {
+                    temp[k++] = inputArray[i++];
+                }
+                else
+                {
+                    temp[k++] = inputArray[j++];
+
+                    
+                    invCount = invCount + (mid - i);
+                }
+            }
+
+            while (i <= mid - 1)
+            {
+                temp[k++] = inputArray[i++];
+            }
+
+            while (j <= right)
+            {
+                temp[k++] = inputArray[j++];
+            }
+
+            for (i = left; i <= right; i++)
+            {
+                inputArray[i] = temp[i];
+            }
+
+            return invCount;
+        }
+
+        public static void Main()
+        {
+            int arrayLength = Convert.ToInt32(Console.ReadLine());
+            int[] inputArray = new int[arrayLength] ;
+            for (int i = 0; i < arrayLength; i++)
+            {
+                inputArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine(mergeSort(inputArray, arrayLength));
+            Console.ReadLine();
+        }
+    }
+}
+
