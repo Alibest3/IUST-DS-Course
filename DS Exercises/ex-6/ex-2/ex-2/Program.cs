9c741d7f3a29d55c7e8221048f29bae2db7f03dc
Alibest3 <alibest3@yahoo.com>
2024-02-29 15:02:54 +0330

    DS exercise 6 added

---
+++ DS Exercises/ex-6/ex-2/ex-2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_2
+{
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                GreedyCoins(Convert.ToInt32(Console.ReadLine()));
+                Console.ReadLine();
+            }
+            public static void GreedyCoins(int money)
+            {
+                int[] coins = { 1, 5, 10 };
+                int len = coins.Length;
+
+                List<int> final = new List<int>();
+
+                int i = len - 1;
+                while(i>=0)
+                {
+                    while (money >= coins[i])
+                    {
+                        money -= coins[i];
+                        final.Add(coins[i]);
+                        
+                    }
+                    i--;
+                }
+
+                Console.WriteLine(final.Count);
+
+            }
+        }
+}
+    
+
---
+++ DS Exercises/ex-6/ex-3/ex-3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] coins = { 1, 3, 4 };
+            int len = coins.Length;
+            Console.WriteLine(CoinsMin(coins, len, Convert.ToInt32(Console.ReadLine())));
+            Console.ReadLine();
+        }
+        public static int CoinsMin(int[] coins, int len, int value)
+        {
+            int[] table = new int[value + 1];
+
+            table[0] = 0;
+
+            for (int i = 1; i <= value; i++)
+            {
+                table[i] = int.MaxValue;
+            }
+            for (int i = 1; i <= value; i++)
+            {
+                for (int j = 0; j < len; j++)
+                    if (coins[j] <= i)
+                    {
+                        int k = table[i - coins[j]];
+                        if (k != int.MaxValue && k < table[i] - 1)
+                            table[i] = k + 1;
+                    }
+            }
+
+            return table[value];
+        }
+    }
+}
