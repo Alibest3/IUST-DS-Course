d20c219dbaee9aae12b493ff557fde42db1347b7
Alibest3 <alibest3@yahoo.com>
2024-02-29 15:01:26 +0330

    DS exercise 2 added

---
+++ DS Exercises/ex-2/ex-2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int counter1 = input.Count(t => t == '(');
+            int counter2 = input.Count(t => t == ')');
+            int counter3 = input.Count(t => t == '[');
+            int counter4 = input.Count(t => t == ']');
+            int counter5 = input.Count(t => t == '{');
+            int counter6 = input.Count(t => t == '}');
+            if (counter1 != counter2)
+            {
+                if (counter1 < counter2)
+                {
+                    Console.WriteLine(input.LastIndexOf(')') + 1);
+                }
+                else
+                {
+                    Console.WriteLine(input.LastIndexOf('(') + 1);
+                }
+                
+            }
+            else if (counter3 != counter4)
+            {
+                if (counter3 < counter4)
+                {
+                    Console.WriteLine(input.LastIndexOf(']') + 1);
+                }
+                else
+                {
+                    Console.WriteLine(input.LastIndexOf('[') + 1);
+                }
+
+            }
+            else if (counter5 != counter6)
+            {
+                if (counter5 < counter6)
+                {
+                    Console.WriteLine(input.LastIndexOf('}') + 1);
+                }
+                else
+                {
+                    Console.WriteLine(input.LastIndexOf('{') + 1);
+                }
+                
+            }
+            else
+            {
+                Console.WriteLine(-1);
+            }
+            Console.ReadLine();
+        }
+    }
+}
