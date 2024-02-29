271670adc765d78def35274ce5b5aabbd747ffbb
Alibest3 <alibest3@yahoo.com>
2024-02-29 15:01:05 +0330

    DS exercise 1 added

---
+++ DS Exercises/ex-1/ex-1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace question6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputLength = int.Parse(Console.ReadLine());
+            int[] inputArray = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+            for (int i = 1; i < inputLength - 1 ; i++)
+            {
+                if (inputArray[i+1] - inputArray[i] > (inputArray[i] - inputArray[i - 1]) )
+                {
+                    Console.WriteLine((inputArray[i + 1] + inputArray[i]) / 2);
+                    break;
+                }
+                if (inputArray[i + 1] - inputArray[i] < (inputArray[i] - inputArray[i - 1]) )
+                {
+                    Console.WriteLine((inputArray[i] + inputArray[i-1]) / 2);
+                    break;
+                }
+                
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
